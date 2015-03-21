404604eec640e274411cd65b8ac05cf2be6d5d08
wtx286712645 <286712645@qq.com>
2015-03-21 11:17:18 +0800

    ValidSudoku.cpp

---
+++ ValidSudoku.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution{
+	public:
+		bool isValidSudoku(vector<vector<char> > &board)
+		{
+			for(int i = 0;i < board.size();i++)
+			{
+				vector<int> tool(9,0);
+				for(int j = 0;j < board.size();j++)
+				{
+					if(board[i][j] != '.')
+					{
+						if(	tool[board[i][j] - '0'])
+						{
+							return false;
+						}
+						else
+						{
+							tool[board[i][j]]++;
+						}
+					}
+				}
+			}
+
+			for(int i = 0;i < board.size();i++)
+			{
+				vector<int> tool(9,0);
+				for(int j = 0;j < board.size();j++)
+				{
+					if(board[i][j] != '.')
+					{
+						if(	tool[board[j][i] - '0'])
+						{
+							return false;
+						}
+						else
+						{
+							tool[board[j][i]]++;
+						}
+					}
+				}
+			}
+			
+			for(int i = 0;i < 3;i++)
+			{
+				for(int j = 0;j < 3;j++)
+				{
+					int r_pre = 3 * i;
+					int c_pre = 3*i; 
+					vector<int> tool(9,0);
+					for(int ii = 0;ii < 3;ii++)
+					{
+						for(int jj = 0;jj < 3;jj++)
+						{
+							if(tool[board[r_pre + ii][c_pre + jj]])
+								return false;
+							else
+								tool[board[r_pre + ii][c_pre + jj]]++;
+						}
+					}
+				}
+			}
+			return true;
+		}
+};
+
+int main()
+{
+
+	Solution s;
+	vector<vector<char> > sudo;
+	for(int i = 0;i < 9;i++)
+	{
+		vector<char> tem;
+		for(int j = 0;j < 9;j++)
+		{
+			char c;
+			cin >> c;
+			tem.push_back(c);
+		}
+		sudo.push_back(tem);
+		cout << endl;
+	}
+	if(s.isValidSudoku(sudo))
+		cout << "yes" << endl;
+	else
+		cout << "no" << endl;
+	return 0;
+}
